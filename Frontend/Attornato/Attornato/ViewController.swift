fe3388183340371bfbf2fe25e598ab82744e7c61
Alok Sahay <alok.sahay87@gmail.com>
2024-09-21 15:33:35 +0800

    Project creation

---
+++ Frontend/Attornato/Attornato/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Attornato
+//
+//  Created by Alok Sahay on 21.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
